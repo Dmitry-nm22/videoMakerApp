f82ee6594813c1c9dc5069bbf92f75ecf4d7add4
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-01-22 14:55:45 +0300

    Updates

---
+++ precache-manifest.e5ff647d03f97642b8ddd49d66d53057.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbb60a3b5c921d8beea9064abe3120c",
+    "url": "/videoMakerApp/index.html"
+  },
+  {
+    "revision": "68ecdfc26678ea5557f1",
+    "url": "/videoMakerApp/static/css/main.73b2f9b2.chunk.css"
+  },
+  {
+    "revision": "f54d4154c169f647407f",
+    "url": "/videoMakerApp/static/js/2.73b12e73.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/videoMakerApp/static/js/2.73b12e73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68ecdfc26678ea5557f1",
+    "url": "/videoMakerApp/static/js/main.f5520cff.chunk.js"
+  },
+  {
+    "revision": "0386d7d88c04a7b8018f",
+    "url": "/videoMakerApp/static/js/runtime-main.36930afb.js"
+  },
+  {
+    "revision": "561b5be0e582e13b25081068e07f918f",
+    "url": "/videoMakerApp/static/media/cover.561b5be0.jpg"
+  },
+  {
+    "revision": "5d6782f8908930f08aff5f5a5669e0eb",
+    "url": "/videoMakerApp/static/media/video.5d6782f8.mp4"
+  },
+  {
+    "revision": "61d48e456c0a37d388ae3cf621e332bd",
+    "url": "/videoMakerApp/static/media/video.61d48e45.png"
+  },
+  {
+    "revision": "f1aa257509574dfbab8e40b14bf73d8e",
+    "url": "/videoMakerApp/static/media/wallet.f1aa2575.png"
+  }
+]);
