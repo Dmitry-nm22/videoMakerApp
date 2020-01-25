fbc76cf42a18c9ddff29f459c359186fcc7c7bbd
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-01-25 14:31:37 +0300

    refactoring code

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Portfolio.module.css';
+import img from '../img/Безымянный.png';
 
 
 
@@ -9,13 +10,21 @@ import s from './Portfolio.module.css';
 function Portfolio() {
     return (
         <div className={s.wraper} >
-            <iframe title="vimeo-player" src="https://player.vimeo.com/video/196745520" width="50%" height="360"
-                    frameBorder="0" allowFullScreen>
-            </iframe>
+            <h1>Портфолио</h1>
+            <p>Каждый проект важен для нас</p>
+            <div className={s.inner}>
+                <div className={s.video}>
 
-            <iframe src="https://player.vimeo.com/video/381523435?color=56bdbd&title=0&byline=0&portrait=0" width="50%"
-                    height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen>
-            </iframe>
+                </div>
+                <div className={s.video}></div>
+            </div>
+
+
+
+
+            {/*<iframe src="https://player.vimeo.com/video/381523435?color=56bdbd&title=0&byline=0&portrait=0" width="50%"*/}
+            {/*        height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen>*/}
+            {/*</iframe>*/}
 
         </div>
     );
